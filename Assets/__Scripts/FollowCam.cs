e28ab4a7f01eb7975780a7777980538e2345c8cc
hberniz <55005292+hberniz@users.noreply.github.com>
2019-10-04 00:12:36 -0400

    reset's after throw

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -39,33 +39,33 @@ public class FollowCam : MonoBehaviour {
         transform.position = destination;
         // Set the orthographicSize of the Camera to keep Ground in view
         this.GetComponent<Camera>().orthographicSize = destination.y + 10;     }
-    /*private void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 destination;
         // If there is no poi, return to P: [0,0,0]
-        if (POI == null)
+        if (poi == null)
         {
             destination = Vector3.zero;
         }
         else
         {
             // Get the position of the poi
-            destination = POI.transform.position;
+            destination = poi.transform.position;
             // If poi is a Projectile, check to see if it's at rest
-            if (POI.tag == "Projectile")
+            if (poi.tag == "Projectile")
             {
                 // if it is sleeping (that is, not moving)
-                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                if (poi.GetComponent<Rigidbody>().IsSleeping())
                 {
                     // return to default view
                     // in the next update
-                    POI = null;
+                    poi = null;
                     return;
                 }
             }
         }
 
-
+        /*
         // Limit the X & Y to Max values
         // the slingshot starts in -x, -y territory, so don't
         // start moving until the projectile gets
@@ -85,5 +85,17 @@ public class FollowCam : MonoBehaviour {
 
         // Set the orthographicSize of the Camera to keep Ground in view
         Camera.main.orthographicSize = destination.y + 10;
-    }*/
+        */
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Retain a destination.z of camZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
 }
